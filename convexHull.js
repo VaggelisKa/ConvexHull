9fb6bbdebfb2edbd84bcba2f88d798068d240b9d
VaggelisKa <e.karavasileiadis@gmail.com>
2020-06-30 23:09:00 +0300

    test

--- convexHull.js
+++ convexHull.js
@@ -44,6 +44,7 @@
       ((points[i+1].y - points[i+2].y) * (points[i].x - points[i+2].x))
     )
 
+  
     console.log('before delete: ' + lowerHull.length);
     console.log('det: ' + determinant)
     while(lowerHull.length > 2 && determinant > 0) {
